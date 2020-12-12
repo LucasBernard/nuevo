e482b39214f33b66045d347ff6430b127d062555
Felipe Villegas <felipe.villegas@sansano.usm.cl>
2020-12-12 15:20:26 -0300

    lista amigos a medio avance

--- src/Componentes/listaAmigos.jsx
+++ src/Componentes/listaAmigos.jsx
@@ -1,8 +1,33 @@
-import React, {useState,useEffect} from "react";
+import React, {useState , useEffect} from "react";
+import { Label, Container, List, Grid, Divider} from 'semantic-ui-react';
 
-function listaAmigos(props) {
+function ListaAmigos(props) {
+    const amigosPersonales = [];
+    
+    function agregarAmigo(usuarioSeleccionado) {
+        amigosPersonales.push(usuarioSeleccionado);
+    }
+
+    function removerAmigo(usuarioSeleccionado, usuarioEliminado) {
+        var indexUsuarioEliminado = amigosPersonales.indexOf(usuarioSeleccionado);
+         
+    }
+
     return(
+        <Container>
+            <Grid>
+                <Grid.Row>
+                    <Label>Amigos</Label>
+                </Grid.Row>
+                <Divider hidden/>
+                <Grid.Row>
+                    <List>
+
+                    </List>
+                </Grid.Row>
+            </Grid>
+        </Container>
     );
 }
 
-export default listaAmigos;
+export default ListaAmigos;
