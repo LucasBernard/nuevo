57f746e7d94f2976febd88ace4868d025dde63ea
LucasBernard <72321668+LucasBernard@users.noreply.github.com>
2020-12-12 22:12:58 -0300

    nuevaspaginas

--- src/Componentes/Login.jsx
+++ src/Componentes/Login.jsx
@@ -1,93 +1,95 @@
 import '../App.css';
 import React, { useState } from 'react';
-import { Container, Grid, Header, Form, Icon, Button, Divider } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Container, Grid, Header, Form, Icon, Button, Divider, Segment, Image, Label } from 'semantic-ui-react';
+import { useHistory, Link } from 'react-router-dom';
 
-const LoginForm = () => {
+const DividerExampleVerticalForm = () => {
 	const password_valido = 1234;
 	const nombre_feli = 'F';
+	const history = useHistory();
 
-	const [ sesion_usuario, setSesion_usuario] = useState('');
-	const [	sesion_password, setSesion_password] = useState('');
+	const [ sesion_usuario, setSesion_usuario ] = useState('');
+	const [ sesion_password, setSesion_password ] = useState('');
 
 	const [ usuario, setUsuario ] = useState('');
 	const [ password, setPassword ] = useState('');
 	const [ mensaje, setMensaje ] = useState('');
 
+
+
+
 	console.log(usuario);
 	console.log(password);
+
+	
 
 	function validacion(usuario, password) {
 		if (usuario == nombre_feli && password == password_valido) {
 			setMensaje('Pronto será redirigido...');
-			localStorage.setItem(usuario, setSesion_usuario)
-			localStorage.setItem(password, setSesion_password)
+			history.push('/Principal');
 		} else {
-			setMensaje('Los datos que ha ingresado son incorrectos.');
+
+			setMensaje('Ingreso los datos de manera incorrecta.');
+			
+
 		}
 	}
-	console.log(sesion_usuario)
-	console.log(sesion_password)
+	console.log(sesion_usuario);
+	console.log(sesion_password);
 
 	return (
-		<Container>
-			<Grid>
-				<Grid.Row textAlign='centered'>
-					<Container fluid>
-						<Icon name="users" size="massive" />
-						<Header size='huge'>Inicio de Sesión</Header>
-					</Container>
-				</Grid.Row>
-				<Divider hidden/>
-				<Grid.Row verticalAlign='middle'>
-					<Container fluid>
-						<Grid>
-							<Grid.Row textAlign='centered'>
-								<Form>
-									<Form.Group>
-										<Form.Input
-											label="Usuario:"
-											name='usuario'
-											control='input'
-											placeholder="Ingrese su usuario"
-											name='usuario'
-											value={usuario}
-											required
-											onChange={(event) => {setUsuario(event.target.value); }}	/>
-										<Form.Input
-											label='Contraseña:'
-											name='contraseña'
-											control='input'
-											placeholder="Ingrese su contraseña"
-											value={password}
-											required
-											onChange={(event) => {setPassword(event.target.value); }}/>
-									</Form.Group>
-									<Divider hidden/>
-									<Grid columns={2} textAlign="left">
-										<Grid.Column>
-											<Link to="/Registro">Si no tiene cuenta, registrese aquí.</Link>
-										</Grid.Column>
-										<Grid.Column>
-											<Button
-												floated='right'
-												primary type="Submit"
-												onClick={() => {validacion(usuario, password);}}>
-												Iniciar Sesión
-											</Button>
-										</Grid.Column>
-									</Grid>
-								</Form>
-							</Grid.Row>
-							<Grid.Row textAlign='centered'>
-								<h2>{mensaje}</h2>
-							</Grid.Row>
-						</Grid>
-					</Container>
-				</Grid.Row>
-			</Grid>
+		
+		<Container style={{ marginTop: '100px' }}>
+			<Image size="tiny" style={{ width: '200px' }} centered="true" src="/logoStudyAnt.png" />
+			<Header style={{marginLeft:"400px"}} as="h1">Bienvenido a Study Ant's</Header>
+			<Segment placeholder>
+				<Grid columns={2} relaxed="very" stackable>
+					<Grid.Column>
+						<Form>
+							<Form.Input
+								icon="user"
+								iconPosition="left"
+								label="Correo"
+								placeholder="Correo"
+								value={usuario}
+								required
+								onChange={(event) => {
+									setUsuario(event.target.value);
+								}}
+							/>
+							<Form.Input
+								icon="lock"
+								iconPosition="left"
+								label="Contraseña"
+								type="password"
+								value={password}
+								required
+								onChange={(event) => {
+									setPassword(event.target.value);
+								}}
+							/>
+							<Label centered="true" >{mensaje}</Label>
+							<Button
+								content="Iniciar Sesión"
+								primary
+								onClick={() => {
+									validacion(usuario, password);
+								}}
+							/>
+						</Form>
+					</Grid.Column>
+
+					<Grid.Column verticalAlign="middle">
+						<Link to="/Registro">
+							<Button content="Registrarse" icon="signup" size="big" />
+						</Link>
+					</Grid.Column>
+				</Grid>
+
+				<Divider vertical>O</Divider>
+			</Segment>
 		</Container>
 	);
 };
 
-export default LoginForm;
+export default DividerExampleVerticalForm;
--- src/Componentes/Principal.jsx
+++ src/Componentes/Principal.jsx
@@ -3,34 +3,12 @@ import '../App.css';
 import { Menu, Grid, Image, Container } from 'semantic-ui-react';
 
 const Principal = () => {
-	const [ sesion_usuario, setSesion_usuario] = useState('');
-	
-	return (
-		<Menu borderless fixed="left" vertical>
-			<Menu.Item as='a' header>
-				<Image size='tiny' centered="true" src='/logoStudyAnt.png'/>
-				Study-Ant
-			</Menu.Item>
-			<Menu.Item as='a'>
-				<i aria-hidden="true" class="home icon" />Inicio
-			</Menu.Item>
-			<Menu.Item as='a'>
-				<i aria-hidden="true" class="user circle icon" />Perfil
-			</Menu.Item>
-			<Menu.Item as='a'>
-				<i aria-hidden="true" class="group icon" />Buscar grupos
-			</Menu.Item>
-			<Menu.Item as='a'>
-				<i aria-hidden="true" class="address book icon" />Grupos
-			</Menu.Item>
-			<Menu.Item as='a'>
-				<i aria-hidden="true" class="book icon" />Biblioteca Personal
-			</Menu.Item>
-			<Menu.Item as='a'>
-				<i aria-hidden="true" class="address card icon" />Amigos
-			</Menu.Item>
-		</Menu>
+	const [ sesion_usuario, setSesion_usuario ] = useState('');
 
+	return (
+		<div className="Contenedor">
+			<h1>Hola</h1>
+		</div>
 		/* Hay que escribir más */
 
 		/* sobretodo del contenedor principal */
